11f4bf24383226b9714f33be9f2eea22773f33a0
Nico Hielscher <63159398+Xenomojin@users.noreply.github.com>
2025-09-14 19:38:04 +0200

    add capstone outline

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ function setup() {
 
 function draw() {
   const TILE_WIDTH = 80;
-  const STONE_WIDTH = 70;
+  const STONE_WIDTH = 65;
   const STONE_HEIGHT = 20;
   const CAPSTONE_RADIUS = 25;
 
@@ -269,49 +269,66 @@ function draw() {
   for (const piece of pieces) {
     push();
     if (piece.kind == "stone") {
+      stroke("#777");
       translate(
         piece.pos[0] * TILE_WIDTH,
         -(piece.pos[2] * STONE_HEIGHT + STONE_HEIGHT / 2),
         piece.pos[1] * TILE_WIDTH,
       );
-      if (piece.color == "black") {
-        fill("black");
-        stroke("white");
-      } else {
-        fill("white");
-        stroke("black");
-      }
+      fill(piece.color);
       box(STONE_WIDTH, STONE_HEIGHT, STONE_WIDTH);
     } else if (piece.kind == "wall") {
+      stroke("#777");
       translate(
         piece.pos[0] * TILE_WIDTH,
         -(piece.pos[2] * STONE_HEIGHT + STONE_WIDTH / 2),
         piece.pos[1] * TILE_WIDTH,
       );
-      if (piece.color == "black") {
-        fill("black");
-        stroke("white");
-      } else {
-        fill("white");
-        stroke("black");
-      }
+      fill(piece.color);
       rotateY(45);
       box(STONE_WIDTH, STONE_WIDTH, STONE_HEIGHT);
     } else {
+      noStroke();
       translate(
         piece.pos[0] * TILE_WIDTH,
         -(piece.pos[2] * STONE_HEIGHT + STONE_WIDTH / 2),
         piece.pos[1] * TILE_WIDTH,
       );
-      if (piece.color == "black") {
-        fill("black");
-        stroke("black");
-      } else {
-        fill("white");
-        stroke("white");
-      }
+      fill(piece.color);
       cylinder(CAPSTONE_RADIUS, STONE_WIDTH);
+
+      stroke("#777");
+
+      push();
+      translate(0, -STONE_WIDTH / 2, 0);
+      drawCircleOutline(CAPSTONE_RADIUS, 24);
+      pop();
+
+      push();
+      translate(0, STONE_WIDTH / 2, 0);
+      drawCircleOutline(CAPSTONE_RADIUS, 24);
+      pop();
     }
     pop();
   }
+}
+
+function drawCircleOutline(radius, segments) {
+  beginShape(LINES);
+  for (let i = 0; i <= segments; i++) {
+    let angle1 = map(i, 0, segments, 0, 360);
+    let angle2 = map(i + 1, 0, segments, 0, 360);
+
+    let x1 = radius * cos(angle1);
+    let y1 = 0;
+    let z1 = radius * sin(angle1);
+
+    let x2 = radius * cos(angle2);
+    let y2 = 0;
+    let z2 = radius * sin(angle2);
+
+    vertex(x1, y1, z1);
+    vertex(x2, y2, z2);
+  }
+  endShape();
 }
